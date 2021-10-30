2e220a319ad596cd2ccbe6176d8f84732ad2e89a
Melissa Passeri <melissapasseri@gmail.com>
2021-10-30 18:25:07 +0200

    Creato ExperienceService

---
+++ Gogo/Controllers/ExperienceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Gogo.ModelData;
+
+namespace Gogo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExperienceController : ControllerBase
+    {
+        public ExperienceController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Experience> Get()
+        {
+            NotFound();
+            return (IEnumerable<Experience>)Array.Empty<object>();
+        }
+    }
+}
---
+++ Gogo/ModelData/Experience.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Gogo.ModelData
+{
+    public class Experience
+    {
+        private DateTime StartDateTime;
+
+        private DateTime EndDateTime;
+
+        private string Name;
+
+        private string Description;
+
+        private string Location;
+
+        public Experience(string n)
+        {
+            Name = n; 
+        }
+
+        public Experience(DateTime date)
+        {
+            StartDateTime = date;
+        }
+
+        public void SetDate(DateTime Start, DateTime End)
+        {
+            StartDateTime = Start;
+            EndDateTime = End;
+        }
+
+        public void SetLocation(string Place)
+        {
+            Location = Place;
+        }
+
+        public void SetDescription(string D)
+        {
+            Description = D;
+        }
+    }
+
+}
