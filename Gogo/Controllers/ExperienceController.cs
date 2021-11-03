b6b00848297e359cd1d5d448853734cf810189fd
Melissa <m.passeri@teamdev.it>
2021-11-03 13:01:26 +0100

    lavorando sulla POST #WIP

--- Gogo/Controllers/ExperienceController.cs
+++ Gogo/Controllers/ExperienceController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Gogo.ModelData;
 using Gogo.Service;
+using Microsoft.AspNetCore.Http;
 
 namespace Gogo.Controllers
 {
@@ -11,16 +12,29 @@ namespace Gogo.Controllers
     public class ExperienceController : ControllerBase
     {
         private IExperienceService _experienceService; // queste tre righe sono la DI(dependency Injection)
+        private HttpContext context;
 
-        public ExperienceController(IExperienceService experienceService)
+        public ExperienceController(IExperienceService experienceService, HttpContext httpContext)
         {
           _experienceService = experienceService;
+            context = httpContext;
         }
+
+        public object HttpServerUtility { get; private set; }
 
         [HttpGet]
         public IEnumerable<ExperienceDTO> Get()
         {
+            return _experienceService.GetAllDTO(); 
+        }
+
+        [HttpPost]
+        public IEnumerable<ExperienceDTO> Post([FromBody] model)
+        {
+
             return _experienceService.GetAllDTO();
         }
+
     }
+
 }
